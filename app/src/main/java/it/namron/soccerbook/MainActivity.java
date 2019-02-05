ad88e4eb48bc3f6acdff9ed62c96b96a277d1815
NamroN <normanargiolas@gmail.com>
2019-02-05 19:57:28 +0100

    first menu inferiore

---
+++ app/src/main/java/it/namron/soccerbook/MainActivity.java
@@ -0,0 +1,47 @@
+package it.namron.soccerbook;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    mTextMessage.setText("Home");
+                    return true;
+                case R.id.cerca_campo:
+                    mTextMessage.setText("Cerca campo");
+                    return true;
+                case R.id.cerca_squadra:
+                    mTextMessage.setText("Cerca squadra");
+                    return true;
+                case R.id.profilo:
+                    mTextMessage.setText("Profilo");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
