218c2e739b7ac6cdf71c0a9ecc2ea6682927165e
NamroN <normanargiolas@gmail.com>
2019-06-17 19:48:48 +0200

    main page

--- app/src/main/java/it/namron/soccerbook/MainActivity.java
+++ app/src/main/java/it/namron/soccerbook/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ImageView homeImg;
+    private ImageView palloneImg;
+    private ImageView magliaImg;
+    private ImageView campoImg;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,9 +49,50 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        homeImg = (ImageView)findViewById(R.id.home_id);
+        homeImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked on Home", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        campoImg = (ImageView)findViewById(R.id.campo_id);
+        campoImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked on Campo", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        palloneImg = (ImageView)findViewById(R.id.pallone_id);
+        palloneImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked on Pallone", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        magliaImg = (ImageView)findViewById(R.id.maglia_id);
+        magliaImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked on Maglia", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+
     }
 
 }
